c4d4081cecf53f16981750ef105430254e234461
jayyijianyi <jianyikok@gmail.com>
2020-12-04 20:43:44 +0800

    password checking

--- week4/scripts.js
+++ week4/scripts.js
@@ -7,3 +7,15 @@ function toUC(){
 
 let elUsername = document.getElementById("userName");
 elUsername.addEventListener("keyup", toUC);
+
+function checkPWLength(){
+    let pw = document.getElementById("password").value;
+    if (pw.length<5){
+        alert("Password must be more than 5 characters.");
+    } else{
+        alert("Password accepted!");
+    }
+}
+
+let elPassword = document.getElementById("password");
+elPassword.onblur = checkPWLength;
