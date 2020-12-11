26783f446e4ac01d0aa67082040d8bffc68764e2
jayyijianyi <jianyikok@gmail.com>
2020-12-11 13:03:09 +0800

    Haversine function

---
+++ week05/geolocation/scripts/geolocation.js
@@ -0,0 +1,19 @@
+
+let elLocate = document.getElementById('locate');
+
+elLocate.addEventListener('click', function(){
+    //check whether geolocation obj is in the browser
+    if(navigator.geolocation){
+
+        navigator.geolocation.getCurrentPosition(function(position){
+            let elLat = document.getElementById('lat');
+            let elLong = document.getElementById('long');
+
+            elLat.innerHTML = "Latitude : "+ position.coords.latitude;
+            elLong.innerHTML = "Longitude : "+ position.coords.longitude;
+        });
+
+    } else{
+        alert("Geolocation is not supported by this browser.");
+    }
+})
